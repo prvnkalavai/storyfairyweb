a03a3f238ff5eea4db007b24842780af69777d73
Praveen Kalavai <praveenkalavai@outlook.com>
2024-11-02 03:33:40 -0400

    made the icon/logo background transparent to fit in well with any background.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header: React.FC = () => (
   <header className="fixed top-0 left-0 right-0 h-36 bg-white/50 backdrop-blur-sm shadow-md z-50">
     <div className="max-w-3xl mx-auto">
-      <img src="./Storyfairy2.png" alt="Logo" className="h-36 w-auto" />
+      <img src="./Storyfairy-logo2.png" alt="Logo" className="h-36 w-auto" />
     </div>
   </header>
 );
