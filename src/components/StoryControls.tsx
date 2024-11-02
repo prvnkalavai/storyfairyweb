15a133f9ae1849aa83d410af46934b92d0d65500
Praveen Kalavai <praveenkalavai@outlook.com>
2024-11-02 13:22:09 -0400

    corrected the width of storydisplay ui component

--- src/components/StoryControls.tsx
+++ src/components/StoryControls.tsx
@@ -20,6 +20,7 @@ export const StoryControls: React.FC<StoryControlsProps> = ({
   isDownloading,
   isSharing
 }) => (
+    <div className="max-w-5xl mx-auto p-4">
   <div className="grid grid-cols-2 gap-2 lg:grid-cols-4 mb-4 max-w-5xl">
     <button
       onClick={onNewStory}
@@ -55,5 +56,6 @@ export const StoryControls: React.FC<StoryControlsProps> = ({
         <span className="hidden sm:inline">Share</span>
       </button>
     )}
+  </div>
   </div>
 );
